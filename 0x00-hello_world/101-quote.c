a363fbf89dccfbab04eece8c436d95726a05c039
Kenneth Uwakwe <kenet.au@gmail.com>
2022-09-09 08:23:45 -0700

    gdgdeg

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - prints the standard errot around output
@@ -8,6 +8,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
 }
