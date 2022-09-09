78a98ee087d1881ce1c78ff902b68452839d113e
Kenneth Uwakwe <kenet.au@gmail.com>
2022-09-09 08:21:20 -0700

    fhhkdf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints the standard errot around output
  *
- * Return: Always 0 (Success)
+ * Return: Always 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
