7be68313afa6507b96e207b7d152354ebe9262a1
Kenneth <kenet.au@gmail.com>
2022-09-13 16:16:44 -0700

    putchar commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "unistd.h"
+
+/**
+ * main - function to print
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char kenneth[7] = {'k', 'e', 'n', 'n', 'e', 't', 'h'};
+	unsigned int c;
+
+	for (c = 0; c < sizeof(kenneth); c++)
+	{
+		_putchar(kenneth[c]);
+	}
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a submission for loops and nested loops for ALX projects
