5c0f2656c553156a93011b3fcf26149bcd57b1f7
Kenneth <kenet.au@gmail.com>
2022-09-20 20:23:31 -0700

    prints in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints in reverse
+ * @s: integer used
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (*(s + c))
+	{
+		c++;
+	}
+	c = c - 1;
+	while (c >= 0)
+	{
+		_putchar(*(s + c));
+		c--;
+	}
+	_putchar('\n');
+}
