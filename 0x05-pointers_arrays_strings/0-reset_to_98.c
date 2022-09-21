ccb57deae452e48ab3b4296ae02b002d5b716dbf
Kenneth <kenet.au@gmail.com>
2022-09-20 20:15:07 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer value 98
+ * @n: serves as the pointer to reset to 98
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
